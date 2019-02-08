97878cf39c0ac07b6df4b7bdac20601ed5dc86f3
Siarhei Rabau <Siarhei_Rabau@epam.com>
2019-02-08 17:26:07 +0300

    Add UpdateAddress

--- WingsOn.Api/Controllers/PersonsController.cs
+++ WingsOn.Api/Controllers/PersonsController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Net;
 using Microsoft.AspNetCore.Mvc;
+using WingsOn.Api.ExceptionHandling;
 using WingsOn.Dal;
 using WingsOn.Domain;
 
@@ -29,6 +31,24 @@ namespace WingsOn.Api.Controllers
         {
             var result = _personRepository.Get(id);
             return Ok(result);
+        }
+
+        [HttpPut]
+        [Route("{id}/[action]")]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(ClientError), (int)HttpStatusCode.BadRequest)]
+        public ActionResult UpdateAddress(int id, string newAddress)
+        {
+            var person = _personRepository.Get(id);
+            if (person == null)
+            {
+                throw new ValidationException("Person does not exist");
+            }
+
+            person.Address = newAddress;
+            _personRepository.Save(person);
+
+            return Ok();
         }
     }
 }
